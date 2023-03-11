546f13a293125f23f6c2b3e98a62c2e9e6c9662e
balintsipos <sipos.balint2000@gmail.com>
2023-03-11 19:29:36 +0100

    Add basic draw functionality on mousedown.

---
+++ script.js
@@ -0,0 +1,26 @@
+const drawingBoard = document.querySelector(".drawing-board");
+let cellCount = 64*64;
+let isDrawing = false;
+
+drawingBoard.addEventListener('mousedown', function(event) {
+    isDrawing = true;
+})
+
+drawingBoard.addEventListener('mouseup', function(event) {
+    isDrawing = false;
+})
+
+function fillGrid(count) {
+    for (let i = 0; i < cellCount; i++) {
+        const div = document.createElement('div');
+        div.classList.add('cell');
+        div.addEventListener('mouseover', (event) => {
+            if (isDrawing === true) {
+                event.target.style.backgroundColor = 'black';
+            }
+        })
+        drawingBoard.appendChild(div);
+    }
+}
+
+fillGrid();
