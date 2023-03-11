aa58718638dbf390405472ceb03b60e3c429e5d5
balintsipos <sipos.balint2000@gmail.com>
2023-03-11 20:23:02 +0100

    Add extra styling and slider.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const drawingBoard = document.querySelector(".drawing-board");
-let cellCount = 64*64;
+let cellCount = 64*64; // default value
 let isDrawing = false;
 
 drawingBoard.addEventListener('mousedown', function(event) {
@@ -19,9 +19,22 @@ function fillGrid(count) {
             if (isDrawing === true) {
                 event.target.style.backgroundColor = 'black';
             }
-        })
+        });
         drawingBoard.appendChild(div);
     }
 }
 
+function clearGrid() {
+    const divs = document.querySelectorAll('.cell');
+    divs.forEach(element => {
+        element.style.backgroundColor = 'white';
+    });
+}
+
+function addClearFunctionality() {
+    const clearBtn = document.querySelector('.clearBtn');
+    clearBtn.addEventListener('click', clearGrid);
+}
+
 fillGrid();
+addClearFunctionality();
